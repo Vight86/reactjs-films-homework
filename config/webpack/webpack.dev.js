150f313afcc70fbd0d78d0cb748bf1fe06c956ef
Vova Pekun <vpekun@gmail.com>
2021-03-14 01:04:08 +0300

    Add webpack-dev-middleware and hot-middlew

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,8 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'development',
+  entry: {
+    app: ['webpack-hot-middleware/client']
+  },
   devtool: 'inline-source-map',
   module: {
     rules: [
@@ -20,4 +24,8 @@ module.exports = merge(common, {
       },
     ]
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
 })
