2cb5d4f4c13853bdaabeaf392dd62c3391b10236
Vova Pekun <vpekun@gmail.com>
2021-03-18 20:42:55 +0300

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const open = require('open');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./config/webpack/webpack.dev.js');
 
 const app = express();
-const config = require('./config/webpack/webpack.dev.js');
 const compiler = webpack(config);
 
 const PORT = process.env.PORT ?? 3000;
@@ -13,12 +14,12 @@ const PORT = process.env.PORT ?? 3000;
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
-  })
+  }),
 );
 
 app.use(webpackHotMiddleware(compiler));
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`Serve is started at port ${PORT}...\n`);
   open(`http://localhost:${PORT}/`);
 });
