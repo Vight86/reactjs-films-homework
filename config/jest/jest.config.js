38c4befd2106a56c9c3201ca29751f4944d0cdae
Vova Pekun <vpekun@gmail.com>
2021-03-29 22:15:00 +0300

    Config Jest to proper test classNames

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'babel-jest',
-    '\\.(scss|css)$': 'babel-jest',
+    '\\.(scss|css)$': 'identity-obj-proxy',
   },
 
 };
