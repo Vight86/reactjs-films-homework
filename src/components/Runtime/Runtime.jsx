73476362756e5b815cc9d12daf5c9774c14f0ac0
Vova Pekun <vpekun@gmail.com>
2021-04-18 21:51:15 +0300

    Add Runtime component

---
+++ src/components/Runtime/Runtime.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styles from './Runtime.scss';
+
+const Runtime = ({ children }) => {
+  let hour = Math.floor(children / 60);
+  hour = hour !== 0 ? `${hour}h` : '';
+  let min = children % 60;
+  min = min !== 0 ? `${min}m` : '';
+
+  return (
+    <span className={styles.runtime}>{`${hour} ${min}`.trim()}</span>
+  );
+};
+
+Runtime.propTypes = {
+  children: propTypes.number,
+};
+
+Runtime.defaultProps = {
+  children: 0,
+};
+
+export default Runtime;
