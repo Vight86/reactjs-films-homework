912f6a8502364991dbb9a8cf59b43d29743aeb42
Vova Pekun <vpekun@gmail.com>
2021-04-24 00:34:20 +0300

    Tweak App structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from './components/Header/index';
 import Logo from './components/Logo/index';
 import SearchForm from './components/SearchForm/index';
 import SearchResultsPage from './pages/SearchResultsPage/index';
-import MovieDetailsPage from './pages/MovieDetailsPage/index';
 import Footer from './components/Footer/index';
 
 const App = () => (
@@ -14,7 +13,6 @@ const App = () => (
       <SearchForm />
     </Header>
     <SearchResultsPage />
-    <MovieDetailsPage />
     <Footer />
   </>
 );
