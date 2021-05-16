35cf643fc498570d09683277acdd7b3ec7a1c881
Vova Pekun <vpekun@gmail.com>
2021-05-16 05:52:15 +0300

    Update SelectButton

--- src/components/SelectButton/__tests__/SelectButton.spec.js
+++ src/components/SelectButton/__tests__/SelectButton.spec.js
@@ -2,20 +2,56 @@ import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import SelectButton from '../index';
 
-const setUp = (genres) => {
-  const renderer = new ShallowRenderer();
-  renderer.render(<SelectButton genres={genres} />);
-  return renderer.getRenderOutput();
-};
+const mockGenres = [
+  { id: 1, name: 'Action' },
+  { id: 2, name: 'Scifi' },
+];
+const mockFiterMovies = jest.fn(() => 'tested');
+
+let renderer;
+let result;
+beforeEach(() => {
+  renderer = new ShallowRenderer();
+  renderer.render(
+    <SelectButton
+      genres={mockGenres}
+      filterMovies={mockFiterMovies}
+    >
+      Title
+    </SelectButton>,
+  );
+  result = renderer.getRenderOutput();
+});
+
+afterEach(() => {
+  renderer = null;
+  result = null;
+});
 
 describe('render SelectButton component', () => {
-  it('render without props', () => {
-    const result = setUp();
+  it('render component correctly', () => {
     expect(result).toMatchSnapshot();
   });
 
-  it('render with props', () => {
-    const result = setUp(['item1', 'item2']);
+  it('render handleClick correctly', () => {
+    const selectOptionButton = result.props.children[2].props.children[1][1].props.children;
+    const mockEvent = {
+      target: {
+        innerHTML: 'tested',
+        dataset: {
+          value: '2',
+        },
+      },
+    };
+    selectOptionButton.props.onClick(mockEvent);
+    result = renderer.getRenderOutput();
+    expect(result).toMatchSnapshot();
+  });
+
+  it('render filterButton opening click correctly', () => {
+    const filterButton = result.props.children[0];
+    filterButton.props.onClick();
+    result = renderer.getRenderOutput();
     expect(result).toMatchSnapshot();
   });
 });
