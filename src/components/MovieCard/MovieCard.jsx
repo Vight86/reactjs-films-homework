6d81b9351edddc6d55a33d9d26b0a13ab22184fb
Vova Pekun <vpekun@gmail.com>
2021-05-16 13:41:06 +0300

    Update MovieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,71 +1,61 @@
 import React, { useState } from 'react';
 import propTypes from 'prop-types';
-import styles from './MovieCard.scss';
-import Button from '../Button/index';
-import Title from '../Title/index';
-import GenreList from '../GenreList/index';
-import Rating from '../Rating/index';
-import Cover from '../Cover/index';
-import MovieSynopsis from '../MovieSynopsis/index';
+import MovieCardGrid from './MovieCardGrid/index';
+import MovieCardTable from './MovieCardTable/index';
 
-const MovieCard = ({ movie }) => {
-  const {
-    title, rating, image, genre, info,
-  } = movie;
-
+const MovieCard = ({
+  movie, genres, isGrid, movieTrailerKey, updateMovieTrailerKey,
+}) => {
   const [isInfoShown, setIsInfoShown] = useState(false);
-  const handleClick = () => setIsInfoShown(!isInfoShown);
+  const handleShowInfo = () => setIsInfoShown(!isInfoShown);
 
-  return (
-    <article className={styles.movieCard}>
-      <section className={styles.poster}>
-        <Cover className="inCard">
-          <img src={image} alt={title} />
-        </Cover>
-        <div className={styles.viewInfo}>
-          <Button className="playButton" />
-          <span className={styles.watchNowText}>Watch Now</span>
-          <Button className="primary" onClick={handleClick}>View Info</Button>
-        </div>
-      </section>
-      <section className={styles.generalInfo}>
-        <Title className="secondary">{title}</Title>
-        <GenreList className="secondary" order={3}>{genre}</GenreList>
-        <Rating className="secondary">{rating}</Rating>
-      </section>
-      <section className={`${styles.infoBlock}${isInfoShown === true ? ` ${styles.infoBlock_active}` : ''}`}>
-        <Button className="closeButton" onClick={handleClick}>&times;</Button>
-        <section className={styles.generalInfo}>
-          <Title className="tertiary">{title}</Title>
-          <GenreList className="secondary" order={3}>{genre}</GenreList>
-          <Rating className="secondary">{rating}</Rating>
-        </section>
-        <MovieSynopsis className="secondary">{info}</MovieSynopsis>
-        <Button className="secondary">Watch Now</Button>
-      </section>
-    </article>
-  );
+  const [isModalOpened, setIsModalOpened] = useState(false);
+
+  const handleModalToggle = (movieId, isOpened) => {
+    const paddingRight = window.innerWidth - document.documentElement.clientWidth;
+    updateMovieTrailerKey(movieId);
+    setTimeout(() => { setIsModalOpened(!isOpened); }, 0);
+    document.body.style.overflow = !isOpened ? 'hidden' : '';
+    document.body.style.paddingRight = !isOpened ? `${paddingRight}px` : '0px';
+  };
+
+  const data = {
+    movie,
+    genres,
+    movieTrailerKey,
+    updateMovieTrailerKey,
+    isInfoShown,
+    handleShowInfo,
+    isModalOpened,
+    handleModalToggle,
+  };
+
+  const movieCard = isGrid ? <MovieCardGrid data={data} /> : <MovieCardTable data={data} />;
+
+  return movieCard;
 };
 
 MovieCard.propTypes = {
   movie: propTypes.shape({
-    // eslint-disable-next-line react/forbid-prop-types
-    image: propTypes.object,
+    adult: propTypes.bool,
+    backdropPath: propTypes.string,
+    genreIds: propTypes.arrayOf(propTypes.number),
+    id: propTypes.number,
+    originalLanguage: propTypes.string,
+    originalTitle: propTypes.string,
+    overview: propTypes.string,
+    popularity: propTypes.number,
+    posterPath: propTypes.string,
+    releaseDate: propTypes.string,
     title: propTypes.string,
-    rating: propTypes.number,
-    genre: propTypes.arrayOf(propTypes.string),
-    info: propTypes.string,
-  }),
-};
-
-MovieCard.defaultProps = {
-  movie: {
-    image: { src: '' },
-    title: '',
-    rating: 0,
-    genre: [],
-    info: '',
-  },
+    video: propTypes.bool,
+    voteAverage: propTypes.number,
+    voteCount: propTypes.number,
+  }).isRequired,
+  genres: propTypes.arrayOf(propTypes.object).isRequired,
+  isGrid: propTypes.bool.isRequired,
+  movieTrailerKey: propTypes.string,
+  updateMovieTrailerKey: propTypes.func,
 };
 
 export default MovieCard;
