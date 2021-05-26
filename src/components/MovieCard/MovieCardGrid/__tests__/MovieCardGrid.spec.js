ffd6cb546bd89e35021c7d9a41a20fedb2afc106
Vova Pekun <vpekun@gmail.com>
2021-05-27 00:01:27 +0300

    Update MovieCargGrid component

--- src/components/MovieCard/MovieCardGrid/__tests__/MovieCardGrid.spec.js
+++ src/components/MovieCard/MovieCardGrid/__tests__/MovieCardGrid.spec.js
@@ -2,58 +2,70 @@ import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import MovieCardGrid from '../index';
 
-const mockHandleModalToggle = jest.fn(() => 'tested');
+let mockUseRouteMatchObject;
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useRouteMatch: jest.fn(() => mockUseRouteMatchObject),
+  Link: jest.fn(),
+}));
 
-const mockData = {
-  movie: {
-    adult: false,
-    backdropPath: '/test.jpg',
-    genreIds: [1, 2],
-    id: 1,
-    originalLanguage: 'en',
-    originalTitle: 'test',
-    overview: 'test',
-    popularity: 1,
-    posterPath: '/test.jpg',
-    releaseDate: '1',
-    title: 'test',
-    video: false,
-    voteAverage: 1,
-    voteCount: 1,
-  },
-  genres: [{
-    id: 1,
-    name: 'Action',
-  },
-  {
-    id: 2,
-    name: 'SciFi',
-  }],
-  movieTrailerKey: 'test',
-  isInfoShown: false,
-  handleShowInfo() { },
-  isModalOpened: false,
-  handleModalToggle() {
-    mockHandleModalToggle();
-  },
-};
-
+let mockHandleModalToggle;
+let mockData;
 let renderer;
 let result;
 
 beforeEach(() => {
+  mockUseRouteMatchObject = { url: '/tested' };
+  mockHandleModalToggle = jest.fn(() => 'tested');
+
+  mockData = {
+    movie: {
+      adult: false,
+      backdropPath: '/test.jpg',
+      genreIds: [1, 2],
+      id: 1,
+      originalLanguage: 'en',
+      originalTitle: 'test',
+      overview: 'test',
+      popularity: 1,
+      posterPath: '/test.jpg',
+      releaseDate: '1',
+      title: 'test',
+      video: false,
+      voteAverage: 1,
+      voteCount: 1,
+    },
+    genres: [{
+      id: 1,
+      name: 'Action',
+    },
+    {
+      id: 2,
+      name: 'SciFi',
+    }],
+    movieTrailerKey: 'test',
+    isInfoShown: false,
+    handleShowInfo() { },
+    isModalOpened: false,
+    handleModalToggle() {
+      mockHandleModalToggle();
+    },
+  };
+
   renderer = new ShallowRenderer();
   renderer.render(<MovieCardGrid data={mockData} />);
   result = renderer.getRenderOutput();
 });
 
 afterEach(() => {
+  mockHandleModalToggle.mockClear();
+  mockData = null;
   renderer = null;
   result = null;
 });
 
 describe('render MovieCardGrid component', () => {
-  it('render component with props', () => {
+  it('render correctly', () => {
     expect(result).toMatchSnapshot();
   });
 
