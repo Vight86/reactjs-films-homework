b33d13677e04b7e606b5bb69262ec32b870b7d6e
Vova Pekun <vpekun@gmail.com>
2021-05-26 23:56:39 +0300

    Update Button component

--- src/components/Button/__tests__/Button.spec.js
+++ src/components/Button/__tests__/Button.spec.js
@@ -2,25 +2,30 @@ import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import Button from '../index';
 
-const setUp = (className, onClick, children) => {
-  const renderer = new ShallowRenderer();
-  renderer.render(<Button className={className} onClick={onClick}>{children}</Button>);
-  return renderer.getRenderOutput();
-};
+let mockOnclick;
+let renderer;
+let result;
+
+beforeEach(() => {
+  mockOnclick = jest.fn();
+  renderer = new ShallowRenderer();
+  renderer.render(<Button className="primary" onClick={mockOnclick}>test</Button>);
+  result = renderer.getRenderOutput();
+});
+
+afterEach(() => {
+  mockOnclick.mockClear();
+  renderer = null;
+  result = null;
+});
 
 describe('render Button component', () => {
-  it('render component without props', () => {
-    const result = setUp();
+  it('render correctly', () => {
     expect(result).toMatchSnapshot();
   });
 
-  it('render component with props', () => {
-    const result = setUp('class', null, 'test');
-    expect(result).toMatchSnapshot();
-  });
-
-  it('render onClick without props', () => {
-    const result = setUp();
-    expect(result.props.onClick()).toMatchSnapshot();
+  it('render onClick event correctly', () => {
+    result.props.onClick();
+    expect(mockOnclick).toHaveBeenCalled();
   });
 });
