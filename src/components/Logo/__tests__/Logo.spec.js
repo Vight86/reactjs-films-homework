8818f08644f75740e80bdfe48ef999e9725efdb0
Vova Pekun <vpekun@gmail.com>
2021-05-26 23:59:08 +0300

    Update Logo propTypes and styles

--- src/components/Logo/__tests__/Logo.spec.js
+++ src/components/Logo/__tests__/Logo.spec.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import Logo from '../index';
 
-const setUp = (theme) => {
-  const renderer = new ShallowRenderer();
-  renderer.render(<Logo theme={theme} />);
-  return renderer.getRenderOutput();
-};
+let renderer;
+let result;
+
+beforeEach(() => {
+  renderer = new ShallowRenderer();
+  renderer.render(<Logo className="test">Title</Logo>);
+  result = renderer.getRenderOutput();
+});
+
+afterEach(() => {
+  renderer = null;
+  result = null;
+});
 
 describe('render Logo component', () => {
-  it('render without props', () => {
-    const result = setUp();
-    expect(result).toMatchSnapshot();
-  });
-
-  it('render with props', () => {
-    const result = setUp('test');
+  it('render correctly', () => {
     expect(result).toMatchSnapshot();
   });
 });
