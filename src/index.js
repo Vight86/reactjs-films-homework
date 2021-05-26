b79f873a18d80f365bf8a1183af6cb4f7b4be651
Vova Pekun <vpekun@gmail.com>
2021-05-27 00:13:32 +0300

    Add Router integrated with Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import styles from './index.scss';
 import App from './App';
 
-import store from './modules/store';
+import store, { history } from './modules/store';
 
 render(
   <Provider store={store}>
-    <App style={styles} />
+    <ConnectedRouter history={history}>
+      <App style={styles} />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
 );
